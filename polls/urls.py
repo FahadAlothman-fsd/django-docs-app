3ae7ecc2364874a686399d008c322d09d2b91aeb
Fahad Alothman <dev.fahad.alothman@gmail.com>
2023-03-11 07:16:16 +0300

    complete part 3

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,13 @@ from django.urls import path
 from . import views
 
 
+question_id_route = "<int:question_id>"
+
+app_name = "polls"
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path("", views.index, name="index"),
+    path(f"specifics/{question_id_route}/", views.detail, name="detail"),
+    path(f"{question_id_route}/results/", views.results, name="results"),
+    path(f"{question_id_route}/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
+from .models import Question
+
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse('Welcome to the index route in the polls app')
+    content_ordered_by_latest = Question.objects.order_by("-pub_date")[:5]
+    return render(
+        request, "polls/index.html", {"latest_polls": content_ordered_by_latest}
+    )
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
